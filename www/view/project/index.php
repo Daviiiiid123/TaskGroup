5150f8683c682642c0078771bf0fdc45b4ea8f36
Daviiiiid123 <61392628+Daviiiiid123@users.noreply.github.com>
2025-11-19 11:11:29 +0100

    .

--- www/view/project/index.php
+++ www/view/project/index.php
@@ -3,10 +3,16 @@
 
 require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
-$view->setFragment("css", '<link rel="stylesheet" href="view/styles/projects.css">');
 
 $projects = $view->getVariable("projects");
 $currentuser = $view->getVariable("currentusername");
+
+// Cargar CSS adicional
+$view->moveToFragment("css");
+?>
+<link rel="stylesheet" href="view/styles/projects.css">
+<?php
+$view->moveToDefaultFragment();
 
 ?><?php if (isset($currentuser)): $view->setVariable("title", i18n("Projects"));?>
     <h1><?php echo i18n("Projects"); ?></h1>
@@ -14,15 +20,16 @@ $currentuser = $view->getVariable("currentusername");
     <?php if (count($projects) == 0): ?>
         <p><?php echo i18n("You have no projects. Create a new project to get started!"); ?></p>
     <?php else: ?>
-        <ul>
+        <div class="project-list">
             <?php foreach ($projects as $project): ?>
-                <li>
-                    <a href="index.php?controller=project&action=view&id=<?php echo $project->getId(); ?>">
-                        <?php echo htmlspecialchars($project->getTitle()); ?>
+                <div class="project-card">
+                    <h3><?php echo htmlspecialchars($project->getTitle()); ?></h3>
+                    <a href="index.php?controller=project&action=view&id=<?php echo $project->getId(); ?>" class="btn btn-primary">
+                        Ver proyecto
                     </a>
-                </li>
+                </div>
             <?php endforeach; ?>
-        </ul>
+        </div>
     <?php endif; ?>
 
     <a href="index.php?controller=project&action=create" class="btn btn-primary">
